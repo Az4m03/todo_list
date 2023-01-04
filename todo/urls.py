225f7c5f13e02dd931deecf033d61af919d82e69
Azam <abdurazakovazam01@gmail.com>
2023-01-04 18:56:57 +0500

    User Registration

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import TodoList, TodoDetail, TodoCreate, TodoUpdate, TodoDelete, Login
+from .views import TodoList, TodoDetail, TodoCreate, TodoUpdate, TodoDelete, Login, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
     path('login/', Login.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', RegisterPage.as_view(), name='register'),
     path('', TodoList.as_view(), name='tasks'),
     path('task/<int:pk>/', TodoDetail.as_view(), name='task'),
     path('task-create', TodoCreate.as_view(), name='task-create'),
